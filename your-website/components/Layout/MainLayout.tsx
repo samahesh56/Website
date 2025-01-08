b05437b6db3c59e54964de2c0f080b60098c8174
samahesh56 <samahesh56@gmail.com>
2025-01-08 15:55:04 -0500

    resizing components

--- your-website/components/Layout/MainLayout.tsx
+++ your-website/components/Layout/MainLayout.tsx
@@ -1,15 +1,26 @@
 // components/Layout/MainLayout.tsx
-import  PageTransition from '../Animation/PageTransition';
-import  Footer from './Footer';
+'use client';
+import PageTransition from '../Animation/PageTransition';
+import Footer from './Footer';
+import { usePathname } from 'next/navigation';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
+
   return (
     <div className="min-h-screen bg-gradient-to-b bg-slate-950 text-gray-100">
-      <PageTransition>
-        <main className="container mx-auto px-4 py-8">
-          {children}
-        </main>
-      </PageTransition>
+      <div className="relative">
+        <PageTransition>
+          <main
+            className={`${
+              !isHomePage ? 'container mx-auto px-4 sm:px-6 lg:px-8 py-24' : ''
+            }`}
+          >
+            {children}
+          </main>
+        </PageTransition>
+      </div>
       <Footer />
     </div>
   );
